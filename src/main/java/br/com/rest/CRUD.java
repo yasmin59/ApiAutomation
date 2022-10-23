92d5929cdb92094756d402fc8ff9302c72a29f39
Yasmin <yasminreyner@gmail.com>
2022-10-23 14:08:19 -0300

    Registrar usuário funcionando.

--- src/main/java/br/com/rest/CRUD.java
+++ src/main/java/br/com/rest/CRUD.java
@@ -1,7 +1,8 @@
 package br.com.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -12,33 +13,37 @@ public class CRUD {
 	
 	@Test
 	public void deveRegistrarUsuario() {
-//		User user = new User("read@teste.com", "read", "read");
 		given()
 			.log().all()
-//			.contentType(ContentType.XML)
-//			.body(user)
+			.formParam("email", "teste@read6.com")
+			.formParam("password", "senha")
+			.formParam("role", "read")
+			.contentType(ContentType.URLENC.withCharset("UTF-8"))
 		.when()
 			.post("http://localhost:5000/api/register")
 		.then()
 			.log().all()
 			.statusCode(201)
-//			.body("user.userEmail", is("read@teste.com"))
-//			.body("user.userPwd", is("read"))
-//			.body("user.role", is("read"))
+			//ajustar forma de validação
+//			.body("html.body.success", is("created"))   
 		;
 		
 	}
 	
+	
 	@Test
 	public void deveFazerLogin() {
+		User user = new User("read@teste.com", "read");
 		
 		given()
 			.log().all()
+			.body(user)
+			.contentType(ContentType.URLENC.withCharset("UTF-8"))
 		.when()
-			.get("http://localhost:5000/api/login")
+			.post("http://localhost:5000/api/login")
 		.then()
 			.log().all()
-			.statusCode(401)
+			.statusCode(200)
 //			.body("user.userEmail", is("read@teste.com"))
 //			.body("user.userPwd", is("read"))
 //			.body("user.role", is("read"))
